df27889affbbe077c6189a8e60bee72ae5ae828f
Nikita0728 <kunwornikita123@gmail.com>
2024-04-21 13:05:46 +0545

    hw

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,11 +3,12 @@ import './Navbar.css'
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import logo2 from '../../assets/QMATO.png'
 export const Navbars = () => {
   return (
     <div >
         <Navbar bg="light" data-bs-theme="light">
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home"><img src={logo2}></img> </Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
